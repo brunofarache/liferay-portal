fbec9490f8251524b130ee345036aadf7e36801e
Bruno Farache <bruno.farache@liferay.com>
2019-06-20 11:50:00 -0300

    useHooks

--- modules/apps/data-engine/data-engine-web/src/main/resources/META-INF/resources/js/components/Pagination.es.js
+++ modules/apps/data-engine/data-engine-web/src/main/resources/META-INF/resources/js/components/Pagination.es.js
@@ -1,30 +1,23 @@
 import ClayPagination from '@clayui/pagination';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 const spritemap = `${Liferay.ThemeDisplay.getPathThemeImages()}/lexicon/icons.svg`;
 
-export default class Pagination extends Component {
-	state = {
-		page: 1
+export default function Pagination(props) {
+	const [page, setPage] = useState(1);
+
+	const handleOnPageChange = (page) => {
+		setPage(page);
+		props.onPageChange(page);
 	}
 
-	handleOnPageChange = (page) => {
-		this.setState({page});
-		this.props.onPageChange(page);
-	}
-
-	render() {
-		const { totalPages } = this.props;
-		const { page } = this.state;
-
-		return (
-			<ClayPagination
-				activePage={page}
-				ellipsisBuffer={0}
-				onPageChange={this.handleOnPageChange}
-				spritemap={spritemap}
-				totalPages={totalPages}
-			/>
-		);
-	}
+	return (
+		<ClayPagination
+			activePage={page}
+			ellipsisBuffer={0}
+			onPageChange={handleOnPageChange}
+			spritemap={spritemap}
+			totalPages={props.totalPages}
+		/>
+	);
 }
--- modules/apps/data-engine/data-engine-web/src/main/resources/META-INF/resources/js/components/SearchInput.es.js
+++ modules/apps/data-engine/data-engine-web/src/main/resources/META-INF/resources/js/components/SearchInput.es.js
@@ -1,30 +1,34 @@
 import ClayIcon from '@clayui/icon';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 const spritemap = `${Liferay.ThemeDisplay.getPathThemeImages()}/lexicon/icons.svg`;
 
-export default class SearchInput extends Component {
-	state = {
-		value: ''
+export default function SearchInput(props) {
+	const [keywords, setKeywords] = useState('');
+
+	const onChange = (event) => {
+		const { value } = event.target;
+		setKeywords(value);
+		props.onSearch(1, value);
 	}
 
-	onChange = (event) => {
-		const { onSearch } = this.props;
-		this.setState({value: event.target.value}, () => onSearch(1, this.state.value));
-	}
+	return (
+		<div class="input-group">
+			<div class="input-group-item">
+				<input
+					value={keywords}
+					onChange={onChange} 
+					aria-label="Search for"
+					class="form-control input-group-inset input-group-inset-after"
+					placeholder="Search..."
+					type="text"/>
 
-	render() {
-		return (
-			<div class="input-group">
-				<div class="input-group-item">
-					<input value={this.state.value} onChange={this.onChange} aria-label="Search for" class="form-control input-group-inset input-group-inset-after" placeholder="Search..." type="text"/>
-					<div class="input-group-inset-item input-group-inset-item-after">
-						<button class="btn btn-unstyled d-none d-md-inline-block" type="button">
-							<ClayIcon spritemap={spritemap} symbol="search" />
-						</button>
-					</div>
+				<div class="input-group-inset-item input-group-inset-item-after">
+					<button class="btn btn-unstyled d-none d-md-inline-block" type="button">
+						<ClayIcon spritemap={spritemap} symbol="search" />
+					</button>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
