53e7a879faf705a255cff41a571108710de89efb
Diego Nascimento <diego.nascimento@liferay.com>
2020-03-04 13:33:06 +0100

    LPS-108987 Add Sidebar Multi Panel support in Data Layout Builder (React side)
    Changes from:
    
    https://github.com/wincent/liferay-portal/pull/160
    
    After resolving conflicts with and separating from:
    
    https://github.com/wincent/liferay-portal/pull/159

--- modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/AppContext.es.js
+++ modules/apps/data-engine/data-engine-taglib/src/main/resources/META-INF/resources/data_layout_builder/js/AppContext.es.js
@@ -21,6 +21,7 @@ import {
 	DELETE_DATA_DEFINITION_FIELD,
 	DELETE_DATA_LAYOUT_FIELD,
 	EDIT_CUSTOM_OBJECT_FIELD,
+	SWITCH_SIDEBAR_PANEL,
 	UPDATE_CONFIG,
 	UPDATE_DATA_DEFINITION,
 	UPDATE_DATA_LAYOUT,
@@ -65,6 +66,8 @@ const initialState = {
 	fieldTypes: [],
 	focusedCustomObjectField: {},
 	focusedField: {},
+	sidebarOpen: false,
+	sidebarPanelId: 'fields',
 };
 
 const addCustomObjectField = ({
@@ -292,6 +295,15 @@ const createReducer = dataLayoutBuilder => {
 						...editedFocusedCustomObjectField,
 						settingsContext,
 					},
+				};
+			}
+			case SWITCH_SIDEBAR_PANEL: {
+				const {sidebarOpen, sidebarPanelId} = action.payload;
+
+				return {
+					...state,
+					sidebarOpen,
+					sidebarPanelId,
 				};
 			}
 			case UPDATE_DATA_DEFINITION: {
