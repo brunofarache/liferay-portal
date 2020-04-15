2ee739c632cf17c1fd62ee30f036bb58cf8e3076
Bruno Farache <bruno.farache@liferay.com>
2020-04-15 13:38:36 -0300

    LPS-109656 Clean up generated code

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/model/impl/DDMFormInstanceReportEntryImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/model/impl/DDMFormInstanceReportEntryImpl.java
@@ -15,22 +15,11 @@
 package com.liferay.dynamic.data.mapping.model.impl;
 
 /**
- * The extended model implementation for the DDMFormInstanceReportEntry service. Represents a row in the &quot;DDMFormInstanceReportEntry&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.dynamic.data.mapping.model.DDMFormInstanceReportEntry</code> interface.
- * </p>
- *
- * @author Brian Wing Shun Chan
+ * @author Marcos Martins
  */
 public class DDMFormInstanceReportEntryImpl
 	extends DDMFormInstanceReportEntryBaseImpl {
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a ddm form instance report entry model instance should use the {@link com.liferay.dynamic.data.mapping.model.DDMFormInstanceReportEntry} interface instead.
-	 */
 	public DDMFormInstanceReportEntryImpl() {
 	}
 
