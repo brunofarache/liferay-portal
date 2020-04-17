48570266f9c13cf011c2177b979b467085e6e064
Marcos <marcos.martins@liferay.com>
2020-04-17 12:19:17 -0300

    LPS-109656 implementing local service

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFormInstanceReportLocalServiceImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFormInstanceReportLocalServiceImpl.java
@@ -14,8 +14,11 @@
 
 package com.liferay.dynamic.data.mapping.service.impl;
 
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceReport;
 import com.liferay.dynamic.data.mapping.service.base.DDMFormInstanceReportLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -28,4 +31,60 @@ import org.osgi.service.component.annotations.Component;
 )
 public class DDMFormInstanceReportLocalServiceImpl
 	extends DDMFormInstanceReportLocalServiceBaseImpl {
+
+	@Override
+	public DDMFormInstanceReport addFormInstanceReport(
+			long formInstanceId, String data, long groupId,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		long formInstanceReportId = counterLocalService.increment();
+
+		DDMFormInstanceReport ddmFormInstanceReport =
+			ddmFormInstanceReportPersistence.create(formInstanceReportId);
+
+		ddmFormInstanceReport.setGroupId(groupId);
+		ddmFormInstanceReport.setCompanyId(serviceContext.getCompanyId());
+		ddmFormInstanceReport.setCreateDate(serviceContext.getModifiedDate());
+		ddmFormInstanceReport.setFormInstanceId(formInstanceId);
+		ddmFormInstanceReport.setData(data);
+
+		return ddmFormInstanceReportPersistence.update(ddmFormInstanceReport);
+	}
+
+	@Override
+	public DDMFormInstanceReport deleteFormInstanceReport(
+		DDMFormInstanceReport ddmFormInstanceReport) {
+
+		ddmFormInstanceReportPersistence.remove(ddmFormInstanceReport);
+
+		return ddmFormInstanceReport;
+	}
+
+	@Override
+	public DDMFormInstanceReport getFormInstanceReport(long formInstanceId)
+		throws PortalException {
+
+		return ddmFormInstanceReportPersistence.findByFormInstanceId(
+			formInstanceId);
+	}
+
+	@Override
+	public DDMFormInstanceReport updateFormInstanceReport(
+			String data, long formInstanceReportId,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		DDMFormInstanceReport ddmFormInstanceReport =
+			ddmFormInstanceReportPersistence.findByPrimaryKey(
+				formInstanceReportId);
+
+		ddmFormInstanceReport.setModifiedDate(
+			serviceContext.getModifiedDate(null));
+
+		ddmFormInstanceReport.setData(data);
+
+		return ddmFormInstanceReportPersistence.update(ddmFormInstanceReport);
+	}
+
 }
